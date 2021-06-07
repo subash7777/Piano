c54d95e2fda64846e0db7b3c05a187dacee1369d
subash7777 <banjadesubash7777@gmail.com>
2021-06-07 10:13:08 +0545

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,18 +3,62 @@ let controls = document.querySelectorAll('.piano__control__option');
 let pianoNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 let keyboardMap = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N'];
 let keys =[];
+let tempoSelect = document.querySelector('.piano__tempo');
+let songSelect = document.querySelector('.piano__song-list');
+let playBtn = document.querySelector('.piano__play-btn');
 
-let happyBirthday = `G4,G4,A4,G4,C4,B4,
-                    G4,G4,A4,G4,D5,C5,
-                    G4,G4,G5,E5,C5,B4,A4,
-                    F5,F5,E5,C5,D5,C5`;
+let happyBirthday = `G4,G4,A4,,G4,,C5,,B4,,,,
+                    G4,G4,A4,,G4,,D5,,C5,,,,
+                    G4,G4,G5,,E5,,C5,,B4,,A4,,,,
+                    F5,F5,E5,,C5,,D5,,C5`;
+let jingleBells =`B3,,B3,,B3,,,,B3,,B3,,B3,,,,
+                    B3,,D4,,G3,,A3,B3,,,,
+                    C4,,C4,,C4,,,,C4,C4,,B3,,B3,,,,
+                    B3,B3,,B3,,A3,,A3,,B3,,A3,,,,D4`;
+let Anthem =`A#3,, A3,, G3,, F3,, G3,, A#3,, C4,, D4,,,,
+            A#3,, A3,, G3,, F3,, G3,, F3,, D3,,,,
+            A#3,, C3,, D3,, F3,, G3,, A#3,, C3,, D3,,,,
+            A#3,, A3,, G3,, F3,, G3,, G3,,,,
+            D3,, D3,, G3,, F3,, G3,, F3,, D3,, D3,, G3,, G3,, E4,, C4,, A#3,, G3,, C4,,,,
+            D4,, C4,, A#3,, C4,, A#3,, G3,, A#3,, G3,, F3,, G3,, F3,, D3,, G3,, D4,, C4,, G3,, A#3,, A#3,, A#3`
 
-let playSong = (notesString, tempo) => {
+let playSong = (notesString, tempo, cb) => {
     let notes = notesString.split(',');
     let currentNote = 0;
+    let mousedown = new Event('mousedown');
+    let mouseup = new Event('mouseup');
+    let btn;
 
-    let interval
+    let interval = setInterval(() => {
+        if(currentNote < notes.length) {
+            if(notes[currentNote].trim() !== '') {
+                if(btn) {
+                    btn.dispatchEvent(mouseup);
+
+                }
+                btn = document.querySelector(`[data-letter-note="${notes[currentNote].trim()}"]`);
+                btn.dispatchEvent(mousedown);
+            }
+
+            currentNote++;
+        } else {
+            btn.dispatchEvent(mouseup);
+            clearInterval(interval);
+            cb();
+        }
+    }, 300 / tempo);
 }
+playBtn.addEventListener('mousedown', () => {
+    let tempo = +tempoSelect.value;
+    let songNum = +songSelect.value;
+    playBtn.disabled = true;
+    let enablePlayBtn = () => playBtn.disabled = false;
+    switch(songNum) {
+        case 1 : playSong(jingleBells, tempo, enablePlayBtn); break
+        case 2 : playSong(happyBirthday, tempo, enablePlayBtn); break
+        case 3 : playSong(Anthem, tempo, enablePlayBtn); break
+    }
+})
 
 let init =() => {
     for(let i = 1; i<=5; i++) { 
@@ -59,22 +103,15 @@ document.addEventListener('keydown', (e) => {
     if(e.repeat) {
         return;
     }
-    let lastLetter = e.code.substring(e.code.length-1);
-    let isShiftPressed = e.shiftKey;
-    let selector;
-    if(isShiftPressed) {
-        selector = `[data-keyboard = "⇧+${lastLetter}"]`;
-    } else {
-        selector = `[data-keyboard = ${lastLetter}]`;
-    }
-    let key = document.querySelector(selector);
-    if(key !== null) {
-        let mousedown = new Event('mousedown');
-        key.dispatchEvent(mousedown);
-    }
+    pressKey('mousedown', e);
 })
 
 document.addEventListener('keyup', (e) => {
+    pressKey('mouseup', e);
+   
+}) 
+
+let pressKey = (mouseEvent, e) => {
     let lastLetter = e.code.substring(e.code.length-1);
     let isShiftPressed = e.shiftKey;
     let selector;
@@ -85,10 +122,11 @@ document.addEventListener('keyup', (e) => {
     }
     let key = document.querySelector(selector);
     if(key !== null) {
-        let mouseup = new Event('mouseup');
-        key.dispatchEvent(mouseup);
+        let event = new Event(mouseEvent);
+        key.dispatchEvent(event);
     }
-})
+
+}
 
 let playSound = (key) => {
     let audio = document.createElement('audio');
